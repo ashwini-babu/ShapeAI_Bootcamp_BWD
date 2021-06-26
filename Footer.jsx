4d5ac43bf49c948a4c07e2686be14ed75a956bdd
ashwini-babu <49549719+ashwini-babu@users.noreply.github.com>
2021-06-26 18:45:54 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Copyright  by ShapeAI @{new Date().getFullYear()}</h1>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>It was really great bootcamp organised by ShapeAI and i had an amazing experince in learning about React.js</p>
+    </div>
+  );
+}
+
+export default Info;
